cead9f604d60b5dc6c143a34a58cc319bb4491c5
Victor Canto <iamvictorcanto@gmail.com>
2023-01-13 16:10:48 -0300

    feat: part 2 done, generating Jwt tokens

---
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,46 @@
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
+
+namespace BuberDinner.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly JwtSettings _jwtSettings;
+    private readonly IDateTimeProvider _dataTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dataTimeProvider, IOptions<JwtSettings> jwtSettings)
+    {
+        _dataTimeProvider = dataTimeProvider;
+        _jwtSettings = jwtSettings.Value;
+
+    }
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+                SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName,firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _dataTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
